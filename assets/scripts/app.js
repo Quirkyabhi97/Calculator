286eb92c14ffa7aaeba78f23ab9d2ffd62a11eb0
Abhinav Sharma <abhi28121997@gmail.com>
2023-03-28 00:10:27 +0530

    Add function added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,10 @@
-alert("This works when index.html file loads");
+const defaultInput = 0;
+let currentResult = defaultInput;
+calculationDescription = "Addition";
+
+const add = () => {
+  currentResult = currentResult + userInput.value;
+  outputResult(currentResult, calculationDescription);
+};
+
+addBtn.addEventListener("click", add);
